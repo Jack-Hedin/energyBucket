cda015d337959436f4126d22327e1ce7c29c18fa
Jack Hedin <jackrhedin+1@gmail.com>
2026-07-23 16:42:16 -0400

    gave it ability to change which event it proccesses

--- macro/Fun4All_energyBucket.C
+++ macro/Fun4All_energyBucket.C
@@ -3,15 +3,10 @@
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/SubsysReco.h>
 
-
-// I copied this from the rtrk one, I dont know which of these I actually need
-
 #include <fun4all/Fun4AllUtils.h>
 #include <fun4all/Fun4AllOutputManager.h>
 #include <fun4all/Fun4AllRunNodeInputManager.h>
 #include <fun4all/Fun4AllDstInputManager.h>
-#include <fun4all/Fun4AllInputManager.h>
-#include <fun4all/Fun4AllServer.h>
 #include <fun4all/SubsysReco.h>
 #include <fun4all/Fun4AllDstOutputManager.h>
 #include <fun4all/Fun4AllOutputManager.h>
@@ -26,9 +21,6 @@
 #include <fstream>
 #include <iostream>
 
-
-
-
 #include <TSystem.h>
 
 // My two modules
@@ -36,28 +28,14 @@
 R__LOAD_LIBRARY(libenergyBucket.so)
 
 R__LOAD_LIBRARY(libfun4all.so)
-
-//I also copied this 
-
-
-R__LOAD_LIBRARY(libjetbase.so)
-R__LOAD_LIBRARY(libjetbackground.so)
-R__LOAD_LIBRARY(libcalo_reco.so)
-R__LOAD_LIBRARY(libffamodules.so)
-
-R__LOAD_LIBRARY(libg4centrality.so)
-R__LOAD_LIBRARY(libg4dst.so)
-R__LOAD_LIBRARY(libjetbase.so)
-R__LOAD_LIBRARY(libg4jets.so)
-R__LOAD_LIBRARY(libtrack_reco.so)
+//R__LOAD_LIBRARY(libcalo_reco.sio)
+//R__LOAD_LIBRARY(libg4dst.so)
 
 
 
-
-
-void Fun4All_energyBucket(const int stop = 1, //when to stop the runs
+void Fun4All_energyBucket(const int Nwait = 1, //when to actually graph
 	        	  const int should_exit = 1, //whether or not to exit root afterwords, 0 to stay
-			  const int is_simfile = false, //If you are using a simulation file like pythia or something, set to 1
+			  const int is_simfile = false, //If you are using a simulation file like pythia or something, set to true
 			  const string &inputListFile0 = "dst_calofitting.list",  //this is the location of the list I want to use
 			  const string dbtag = "ProdA_2024" //Should be in the file names
 			  ) {
@@ -82,9 +60,9 @@ void Fun4All_energyBucket(const int stop = 1, //when to stop the runs
   recoConsts *rc = recoConsts::instance();
   rc->set_StringFlag("CDB_GLOBALTAG",dbtag);
   rc->set_uint64Flag("TIMESTAMP",runnumber);
-  CDBInterface::instance() -> Verbosity(1);
-  
+  CDBInterface::instance() -> Verbosity(0);
   }
+  
 
 
   //register the input, this seems to be the standard way to do thsi
@@ -98,21 +76,25 @@ void Fun4All_energyBucket(const int stop = 1, //when to stop the runs
   // - Garrus Vakarian
   Process_Calo_Calib();
 
-  //MBD Reconstruction for vertex stuff ig
-  MbdReco *mbdreco = new MbdReco();
-  se->registerSubsystem(mbdreco);
 
-  GlobalVertexReco *gvertex = new GlobalVertexReco();
-  gvertex->Verbosity(Fun4AllBase::VERBOSITY_QUIET);
-  se->registerSubsystem(gvertex);
+  //only needed if you need the vertex or MBD stuff
+  //MBD Reconstruction for vertex stuff ig
+  //MbdReco *mbdreco = new MbdReco();
+  //se->registerSubsystem(mbdreco);
+
+  //GlobalVertexReco *gvertex = new GlobalVertexReco();
+  //gvertex->Verbosity(Fun4AllBase::VERBOSITY_QUIET);
+  //se->registerSubsystem(gvertex);
   }
 
-
+  
   energyBucket *myEB = new energyBucket("energyBucket.root");
+  myEB->set_wait(Nwait);
   se->registerSubsystem(myEB);
+  
 
   //Run some number of events, 0 to run all in the list
-  se->run(stop);
+  se->run(Nwait);
   se->End();
 
   //exits if you want it to, defaults to yes
